0d811cab6a4b08c3e3172ff777e18a6e9c376813
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2019-05-12 12:04:45 +0200

    z3_12052019

--- arkanoid/scripts/game.js
+++ arkanoid/scripts/game.js
@@ -33,43 +33,58 @@
     bricksElement.innerHTML = '<div class="brick"></div>'.repeat(30);
 
     let deltaX = 1;
-    let deltaY = 1;
-    setInterval(function () {
-        const {
-            offsetWidth: arenaWidth,
-            offsetHeight: arenaHeight
-        } = arenaElement;
-/*         const arenaWidth = arenaElement.offsetWidth;
-        const arenaHeight = arenaElement.offsetHeight; */
+    let deltaY = -1;
 
-        if (ballElement.offsetLeft >= arenaWidth - ballElement.offsetWidth) {
-            deltaX = -1;
-        }
+    function ballStart() {
+        setInterval(function () {
+            const {
+                offsetWidth: arenaWidth,
+                offsetHeight: arenaHeight
+            } = arenaElement;
+    /*         const arenaWidth = arenaElement.offsetWidth;
+            const arenaHeight = arenaElement.offsetHeight; */
 
-        if (ballElement.offsetLeft === 0) {
-            deltaX = 1;
-        }
+            if (ballElement.offsetLeft >= arenaWidth - ballElement.offsetWidth) {
+                deltaX = -1;
+            }
 
-        if (ballElement.offsetTop >= arenaHeight - ballElement.offsetWidth) {
-            deltaY = -1;
-        }
+            if (ballElement.offsetLeft === 0) {
+                deltaX = 1;
+            }
 
-        if (ballElement.offsetTop >= paddleElement.offsetTop - ballElement.offsetWidth) {
-            if (ballLeft > paddleLeft && ballLeft < paddleLeft + paddleElement.offsetWidth) {
+            if (ballElement.offsetTop >= arenaHeight - ballElement.offsetWidth) {
                 deltaY = -1;
             }
-        }
 
-        if (ballElement.offsetTop === 0) {
-            deltaY = 1;
-        }
+            if (ballElement.offsetTop >= paddleElement.offsetTop - ballElement.offsetWidth) {
+                if (ballLeft > paddleLeft && ballLeft < paddleLeft + paddleElement.offsetWidth) {
+                    deltaY = -1;
+                }
+            }
 
-        ballTop = ballElement.offsetTop + deltaY;
-        ballLeft = ballElement.offsetLeft + deltaX;
+            if (ballElement.offsetTop === 0) {
+                deltaY = 1;
+            }
 
-        ballElement.style.top = `${ballTop}px`;
-        ballElement.style.left = `${ballLeft}px`;
-    }, 10);
+            ballTop = ballElement.offsetTop + deltaY;
+            ballLeft = ballElement.offsetLeft + deltaX;
+
+            ballElement.style.top = `${ballTop}px`;
+            ballElement.style.left = `${ballLeft}px`;
+        }, 10);
+    }
+
+    function setBallStartPosition() {
+        const {
+            offsetLeft: paddleLeft,
+            offsetWidth: paddleWidth,
+            offsetTop: paddleTop
+        } = paddleElement;
+        const {offsetWidth: ballWidth} = ballElement;
+
+        ballElement.style.left = `${paddleLeft + (paddleWidth / 2) - (ballWidth / 2)}px`;
+        ballElement.style.top = `${paddleTop - ballWidth}px`;
+    }
 
     const onMouseMove = function (e) {
         const {left: arenaLeft, right: arenaRight} = arenaElement.getBoundingClientRect();
@@ -87,6 +102,7 @@
             e.stopPropagation();
             e.preventDefault();
 
+            ballStart();
 
             document.addEventListener(
                 'mousemove',
@@ -115,6 +131,8 @@
     gameElement.appendChild(panelElement);
     gameElement.appendChild(arenaElement);
     document.body.appendChild(gameElement);
+
+    setBallStartPosition();
 
     function pannelElement({text, value}) {
         const wrapper = document.createElement('div');
