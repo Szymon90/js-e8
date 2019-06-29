5f50206e92fea85a69a04be4b490fa2537046efc
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2019-06-29 12:49:14 +0200

    zaj

--- jse8-app/src/components/Sections/Sections.js
+++ jse8-app/src/components/Sections/Sections.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import withFetchData from '../withFetchData';
 import { fetchDataSuccess } from './actions';
 
 const path = '/collections?page=1&perPage=10&orderBy=latest';
+
+function Sections({ sections }) {
+    return sections.length && (
+        <Wrapper>
+            {sections.map(
+                (item, index) => (
+                    <Section>
+                        <h3>{item.title}</h3>
+                        <p>{item.description}</p>
+                    </Section>
+                )
+            )}
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    margin-top: 86px
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 250px);
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
+`
+
+const Section = styled.div`
+    background-color: #ffffff;
+    border: 1px solid #000;
+
+`
+
+const mapStateToProps = ({sections }) => ({
+    sections
+})
 
 const mapDispatchToProps = dispatch => ({
     fetchDataSuccess: compose(dispatch, fetchDataSuccess)
@@ -12,10 +46,11 @@ const mapDispatchToProps = dispatch => ({
 
 const mergeProps = (fromState, fromDispatch) => ({
     path,
+    ...fromState,
     ...fromDispatch
 })
 
 export default compose(
-    connect(null, mapDispatchToProps, mergeProps),
+    connect(mapStateToProps, mapDispatchToProps, mergeProps),
     withFetchData
-)((props) => {console.log(props); return (<div></div>)})
+)(Sections)
